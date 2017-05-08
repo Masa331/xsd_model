859f1fd227bef2a9eebcba5628c805653a9060cd
Premysl Donat <pdonat@seznam.cz>
2017-05-08 12:18:08 +0200

    wip

--- lib/xsd_model.rb
+++ lib/xsd_model.rb
@@ -3,54 +3,61 @@ require 'nokogiri'
 require 'active_model'
 
 module XsdModel
-  def build_with_xsd(path)
-    file = File.open(path)
-    @@schema = Nokogiri::XML(file)
-
-    include_active_model
-    define_accessors
-    define_to_xml
-    define_from_xml
-
-    schema
-  end
-
-  private
-
-  def schema
-    @@schema
-  end
-
-  def root_element
-    schema.xpath('xs:schema/xs:element').first
-  end
-
-  def include_active_model
-    include ActiveModel::Model
-  end
-
-  def define_to_xml
-    define_method(:to_xml) do
-      <<~XML
-       <?xml version="1.0" encoding="UTF-8"?>
-       <name>Premysl Donat</name>
-       XML
+  module ClassMethods
+    def schema
+      class_variable_get(:@@schema)
     end
-  end
 
-  def define_from_xml
-    define_singleton_method(:from_xml) do |xml|
+    def from_xml(xml)
       new(name: 'Premysl Donat')
     end
-  end
 
-  def define_accessors
-    if root_element.key? 'type'
-      define_accessor(root_element['name'])
+    def build_with_xsd(path)
+      file = File.open(path)
+      class_variable_set(:@@schema, Nokogiri::XML(file))
+
+      include_active_model
+      define_accessors
+
+      schema
+    end
+
+    def include_active_model
+      include ActiveModel::Model
+    end
+
+    def define_accessors
+      if root_element.key? 'type'
+        define_accessor(root_element['name'])
+      end
+    end
+
+    def define_accessor(name)
+      attr_accessor name
+    end
+
+    def root_element
+      schema.xpath('xs:schema/xs:element').first
     end
   end
 
-  def define_accessor(name)
-    attr_accessor name
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def schema
+    self.class.schema
+  end
+
+  def to_xml
+    doc = Nokogiri::XML::Document.new
+    if self.class.root_element.key? 'type'
+      node = Nokogiri::XML::Node.new(self.class.root_element['name'], doc)
+      node.content = send(self.class.root_element['name'])
+
+      doc << node
+    end
+
+    doc
   end
 end
--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -3,39 +3,39 @@ require_relative 'test_classes/name'
 
 describe Name do
   it 'it has all accessors defined in xsd' do
-    note = Name.new
+    name = Name.new
 
-    expect(note).to have_accessor :name
+    expect(name).to have_accessor :name
   end
 
   context 'validations are performed through validators on model itself' do
     it 'is valid with anything in name' do
-      note = Name.new
+      name = Name.new
 
-      expect(note).to be_valid
+      expect(name).to be_valid
     end
 
     it 'is valid with blank name' do
-      note = Name.new(name: 'Premysl Donat')
+      name = Name.new(name: 'Premysl Donat')
 
-      expect(note).to be_valid
+      expect(name).to be_valid
     end
   end
 
   describe '#to_xml' do
     it "outputs model in xml valid by it's xsd" do
-      note = Name.new(name: 'Premysl Donat')
+      name = Name.new(name: 'Premysl Donat')
 
-      expect(note.to_xml).to eq File.read('./spec/xml/name.xml')
+      expect(name.to_xml.to_xml).to eq File.read('./spec/xml/name.xml')
     end
   end
 
   describe '::from_xml' do
     it 'intializes new instance from given xml' do
       xml = File.read('./spec/xml/name.xml')
-      note = Name.from_xml(xml)
+      name = Name.from_xml(xml)
 
-      expect(note.name).to eq 'Premysl Donat'
+      expect(name.name).to eq 'Premysl Donat'
     end
   end
 end
